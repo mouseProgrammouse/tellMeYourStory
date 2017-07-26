f507ec7c0f594746eb9e7b1d49565cdf5c81c0b3
Alena <mouse.programmouse@gmail.com>
2017-07-26 21:33:56 +0100

    Post adding

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
+from datetime import datetime, date
 from flask.ext.wtf import Form
+import flask.ext.wtf.file
 import wtforms
-
 
 class LoginForm(Form):
     login = wtforms.TextField('login', validators=[wtforms.validators.Required()])
@@ -14,3 +15,10 @@ class RegistrationForm(Form):
     login =  wtforms.TextField('login', validators=[wtforms.validators.Required(), wtforms.validators.Length(min=4, max=25)])
     password =  wtforms.PasswordField('password', validators=[wtforms.validators.Required(), wtforms.validators.EqualTo('confirm', message='Passwords must match')])
     confirm =  wtforms.PasswordField('repeat password', validators=[wtforms.validators.Required()])
+
+
+class CreatePost(Form):
+    title = wtforms.TextField(render_kw={"placeholder": "title"}, validators=[wtforms.validators.Required()])
+    text = wtforms.TextAreaField(render_kw={"placeholder": "write your story"}, validators=[wtforms.validators.Required()])
+    date = wtforms.DateTimeField(render_kw={"placeholder": "format: YYYY-mm-dd HH:MM"}, default=datetime.now, format='%Y-%m-%d %H:%M', validators=[wtforms.validators.Required()])
+    image = wtforms.FileField(validators=[flask.ext.wtf.file.FileRequired(), flask.ext.wtf.file.FileAllowed(['png', 'jpg', 'jpeg'], message='Wrong file type')])
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from app import app, bcrypt, db, login_manager
-from forms import LoginForm, RegistrationForm
+from forms import LoginForm, RegistrationForm, CreatePost
 from flask import flash, g, redirect, render_template, request, session, url_for 
 from flask.ext.login import current_user, login_required, login_user, logout_user
 from models import Posts, User
+from werkzeug.utils import secure_filename
+import calendar, os, time
 
 
 @app.login_manager.user_loader
@@ -21,7 +23,8 @@ def before_request():
 @login_required
 def index():
     """ Start page """
-    user = { 'login': g.user.login}
+    user = { 'name':g.user.name,
+        'surname':g.user.surname}
     return render_template('index.html',
         title='test',
         user=user)
@@ -84,4 +87,36 @@ def registration():
             return redirect(url_for("login"))
     return render_template('registration.html',
         title='Registration',
+        form=form)
+
+
+@app.route('/addpost', methods=['GET', 'POST'])
+@login_required
+def add_post():
+    form = CreatePost()
+    if request.method == 'POST' and form.validate_on_submit():
+        """ Get image info """
+        image = form.image.data
+        """ Create new file name """
+        old_filename, extension = os.path.splitext(image.filename)
+        filename = str(int(calendar.timegm(time.gmtime()))) + extension
+        """ Check directory """
+        directory = os.path.join(app.config['UPLOAD_FOLDER'], str(g.user.get_id()))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        """ Save image """
+        image.save(os.path.join(directory, filename))
+        """ Add post to DB """
+        new_post = Posts(user_id=g.user.get_id(),
+            title=form.title.data,
+            text=form.text.data,
+            pub_date=form.date.data,
+            img=filename)
+        db.session.add(new_post)
+        db.session.commit()
+        """ Success message """
+        flash('Done')
+        return (redirect(url_for("index")))
+    return render_template('addpost.html',
+        title='Create new post',
         form=form)
